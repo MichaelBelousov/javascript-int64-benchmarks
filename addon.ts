3a4201c2ff4c0bed935f93e8746cd2c08868de04
Michael Belousov <mike.belousov@bentley.com>
2022-03-20 10:17:04 -0400

    add eq function for Id64Args

--- addon.ts
+++ addon.ts
@@ -52,5 +52,46 @@ export function doubleAsBufferEq(...[l, r]: [...Id64Arg.DoubleAsBuffer, ...Id64A
     : l === r;
 }
 
+export function eq(kind: Id64ArgKind.LowHighObject,  ...[l, r]: [...Id64Arg.LowHighObject, ...Id64Arg.LowHighObject]):  boolean
+export function eq(kind: Id64ArgKind.LowHighArray,   ...[l, r]: [...Id64Arg.LowHighArray, ...Id64Arg.LowHighArray]):   boolean
+export function eq(kind: Id64ArgKind.HexString,      ...[l, r]: [...Id64Arg.HexString, ...Id64Arg.HexString]):      boolean
+export function eq(kind: Id64ArgKind.Base64String,   ...[l, r]: [...Id64Arg.Base64String, ...Id64Arg.Base64String]):   boolean
+export function eq(kind: Id64ArgKind.ByteString,     ...[l, r]: [...Id64Arg.ByteString, ...Id64Arg.ByteString]):     boolean
+export function eq(kind: Id64ArgKind.TwoNumbers,     ...[l, r]: [...Id64Arg.TwoNumbers, ...Id64Arg.TwoNumbers]):     boolean
+export function eq(kind: Id64ArgKind.Uint32Array,    ...[l, r]: [...Id64Arg.Uint32Array, ...Id64Arg.Uint32Array]):    boolean
+export function eq(kind: Id64ArgKind.DoubleAsBuffer, ...[l, r]: [...Id64Arg.DoubleAsBuffer, ...Id64Arg.DoubleAsBuffer]): boolean
+export function eq(kind: Id64ArgKind.BigInt,         ...[l, r]: [...Id64Arg.BigInt, ...Id64Arg.BigInt]):         boolean
+export function eq(...[kind, l, r, r1, r2]:
+  | [Id64ArgKind.LowHighObject, ...Id64Arg.LowHighObject, ...Id64Arg.LowHighObject]
+  | [Id64ArgKind.LowHighArray,  ...Id64Arg.LowHighArray, ...Id64Arg.LowHighArray]
+  | [Id64ArgKind.HexString,     ...Id64Arg.HexString, ...Id64Arg.HexString]
+  | [Id64ArgKind.Base64String,  ...Id64Arg.Base64String, ...Id64Arg.Base64String]
+  | [Id64ArgKind.ByteString,    ...Id64Arg.ByteString, ...Id64Arg.ByteString]
+  | [Id64ArgKind.TwoNumbers,    ...Id64Arg.TwoNumbers, ...Id64Arg.TwoNumbers]
+  | [Id64ArgKind.Uint32Array,   ...Id64Arg.Uint32Array, ...Id64Arg.Uint32Array]
+  | [Id64ArgKind.DoubleAsBuffer,...Id64Arg.DoubleAsBuffer, ...Id64Arg.DoubleAsBuffer]
+  | [Id64ArgKind.BigInt,        ...Id64Arg.BigInt, ...Id64Arg.BigInt]
+): boolean {
+  switch (kind) {
+    case Id64ArgKind.LowHighObject:
+      return l.low === r.low && l.high === r.high;
+    case Id64ArgKind.Uint32Array:
+    case Id64ArgKind.LowHighArray:
+      return l[0] === r[0] && l[1] === r[1];
+    case Id64ArgKind.TwoNumbers:
+      return l === r1 && r === r2;
+    case Id64ArgKind.TwoNumbers:
+      return l === r1 && r === r2;
+    case Id64ArgKind.DoubleAsBuffer:
+      // there is a small proportion (though not extremely tiny) of random u64 that will be NaN and because of the js spec we must handle these in native
+      return Number.isNaN(l) || Number.isNaN(r)
+        // must resort to native check when either is NaN
+        ? doubleAsBufferWhenNanEqFallback(l, r)
+        : l === r;
+    default:
+      return l === r;
+  }
+}
+
 export declare function getLastHighBits(): number;
 
