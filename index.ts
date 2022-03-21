1e714e866322ae80cdf05d86947f641e86ffa983
Michael Belousov <mike.belousov@bentley.com>
2022-03-20 21:13:48 -0400

    layout of ts end

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
 import * as Benchmark from "benchmark";
-import * as addon from "./addon";
+import { Id64ArgKind } from "./addon";
 
 const suite = new Benchmark.Suite();
+
+function randUint32() {
+  const MAX_UINT32 = 2**32 - 1;
+  return Math.floor(Math.random() * MAX_UINT32);
+}
 
 // generating the graph in javascript is interesting but afaik not comparable
 // to C++, the random generation is probably pretty non-comparable
@@ -20,18 +25,32 @@ function randId64(kind: Id64ArgKind) {
   }
 }
 
+function buildGraph() {
+  const uint32 = Math.random() * 2**32
+}
+
+function djikstras() {
+
+}
+
 suite
-  .add("low/high u32: {u32,u32}", function() {
+  .add("use low/high object {low: u32, high: u32}", function() {
   })
-  .add("hex: 0xff", function() {
+  .add("use low/high array {u32, u32}", function() {
   })
-  .add("base64: 0xff", function() {
+  .add("use hex string: '0xff'", function() {
   })
-  .add("store u64 directly in string", function() {
+  .add("use base64 string: 'bXd='", function() {
   })
-  .add("store u64 directly in number", function() {
+  .add("use byte string: '\\u{0001}\\x00\\x00\\x42", function() {
   })
-  .add("store u64 directly in number, native equality fallback only", function() {
+  .add("use two numbers", function() {
+  })
+  .add("Uint32Array", function() {
+  })
+  .add("use 64-bit number as an 8-byte buffer", function() {
+  })
+  .add("use 64-bit number as an 8-byte buffer, native equality check only", function() {
   })
   .on("cycle", function(event: Benchmark.Event) {
     console.log(`${event.target}`);
