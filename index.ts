54f1a4b85a912386b773155aaea6893624034106
Michael Belousov <mike.belousov@bentley.com>
2022-03-20 21:12:59 -0400

    add  start of rand generation in javascript which I'll immediately revert

--- index.ts
+++ index.ts
@@ -3,6 +3,23 @@ import * as addon from "./addon";
 
 const suite = new Benchmark.Suite();
 
+// generating the graph in javascript is interesting but afaik not comparable
+// to C++, the random generation is probably pretty non-comparable
+function randId64(kind: Id64ArgKind) {
+  switch (kind) {
+    case Id64ArgKind.LowHighObject: return { low: randUint32(), high: randUint32() };
+    case Id64ArgKind.LowHighArray:
+      return [randUint32(), randUint32()];
+    case Id64ArgKind.HexString:
+      return `0x${randUint32().toString(16)}${randUint32().toString(16).padStart(8, '0')}`
+    case Id64ArgKind.Base64String:
+      return Buffer.from(new Uint32Array([randUint32(), randUint32()])).toString("base64");
+    case Id64ArgKind.ByteString:
+    default:
+      return Math.random();
+  }
+}
+
 suite
   .add("low/high u32: {u32,u32}", function() {
   })
